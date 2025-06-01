04e022c31208349db0eea9d3f760fbf2b9c0f7ad
François Hissel <francois.hissel@gmail.com>
2025-06-01 23:01:54 +0200

    Autocomplete command
    - Autocomplete sub-commands names
    - Allow to use the unnamed register when no register name is given

--- plugin/simplegemini.lua
+++ plugin/simplegemini.lua
@@ -8,16 +8,17 @@ vim.api.nvim_create_user_command('SimpleGemini', function(args)
 	if cmd == 'Scratch' then
 		-- Send answer to new scratch window if the bang-form was used
 		require('simplegemini').create_scratch_with_lines(lines)
-	elseif cmd:sub(1, 4) == 'Reg=' then
+	elseif cmd:sub(1, 3) == 'Reg' then
 		-- Send answer to provided register
-		vim.fn.setreg(cmd:sub(5), lines)
+		local reg = (cmd:len() < 5) and '"' or cmd:sub(5, 5)
+		vim.fn.setreg(reg, lines)
 		print("Register filled with Gemini answer")
 	elseif cmd == 'Buffer' then
 		-- Send answer to current buffer position otherwise
 		local line = vim.api.nvim_win_get_cursor(0)
 		if args.range > 0 and args.bang then
 			-- If Bang version is used, delete selection
-			vim.api.nvim_buf_set_lines(0, line[1]-1, line[1], true, {})
+			vim.api.nvim_buf_set_lines(0, args.line1 - 1, args.line2, true, {})
 		end
 		vim.api.nvim_buf_set_lines(0, line[1]-1, line[1]-1, true, lines)
 	else
@@ -28,4 +29,15 @@ end, {
 		nargs = '?',
 		range = true,
 		bang = true,
+		complete = function(_, cmdline, _)
+			local m = cmdline:match("^.*SimpleGemini%s*(%S*)$")
+			if not m then return nil ; end
+			local res = {}
+			for _, v in pairs({"Scratch ", "Reg=", "Buffer "}) do
+				if v:match("^" .. m) then
+					table.insert(res, v)
+				end
+			end
+			return res
+		end
 	})
