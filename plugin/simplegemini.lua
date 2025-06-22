5ccaf05e46032bbfd214541103823e492993a447
François Hissel <francois.hissel@gmail.com>
2025-06-22 22:42:56 +0200

    Allow to add new LLMs and models
    - Added Groq

--- plugin/simplegemini.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command('SimpleGemini', require('simplegemini').process, {
-		desc = 'Interact with simpleGemini',
-		nargs = '?',
-		range = true,
-		bang = true,
-		complete = require('simplegemini').complete,
-	})
